8b87ad8a7429490666cbd4d29487c77205687946
sivaabhilash474 <sivaabhilash474@gmail.com>
2018-12-26 20:42:09 -0500

    LOGIN PAGE

---
+++ datadriventesting/src/main/java/org/company/projectname/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package org.company.projectname.pages;
+
+public class LoginPage {
+	
+	
+	private static String inputField = "//input[@id='%s']";
+	private static String loginButton = "//input[@id='u_0_2']";
+//	private static String passwordField = "";
+	
+	
+	
+	
+	public void clickOnLogin(String username, String password){
+		
+		
+		
+	}
+	
+	
+	
+}
